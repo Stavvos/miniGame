19a3c158126ee2ec8f73bce1aeb1940331b38fed
liam henry <liam_burridgeuc@hotmail.com>
2025-08-26 18:42:19 +1000

    updated player movement using a state machine. STILL NEEDS IMPROVEMENT

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include "raylib.h"
 #include "math.h"
 #include "stdio.h"
-#define M_PI 3.14159265358979323846
+
+
+//placed this up here to enable printing player state 
+typedef enum PlayerMoveState {MOVEFORWARD, MOVEBACKWARD, MOVELEFT, MOVERIGHT, NOMOVE} PlayerMoveState;
+PlayerMoveState playerMoveState = NOMOVE; 
+
+//used to print a meaningful player state rather than a number
+const char* getPlayerMoveStateString(PlayerMoveState state){
+  switch (state) 
+  {
+    case MOVEFORWARD: return "MOVEFORWARD";
+    case MOVEBACKWARD: return "MOVEBACKWARD";
+    case MOVELEFT: return "MOVELEFT";
+    case MOVERIGHT: return "MOVERIGHT";
+    case NOMOVE: return "NOMOVE";
+    default: return "NULLSTATE";
+  }	
+}	
+	
 
 int main(void)
 {
@@ -19,11 +37,8 @@ int main(void)
 
     //player variables
     Vector2 playerPos = {300.f, 280.f};
-    float playerRotation = M_PI / 4;
-    float playerRotationState = 0.0f;
-    float playerScale = 1.0f;
 
-    //this texture has to be declared after the InitWindow() call due to open gl
+    //texture has to be declared after the InitWindow() call due to open gl
     Texture2D player = LoadTexture("assets/sprite/player/ship.png");
     Texture2D asteroid = LoadTexture("assets/sprite/world/asteroid.png");
     Texture2D asteroidMedium = LoadTexture("assets/sprite/world/asteroidMedium.png");
@@ -41,7 +56,7 @@ int main(void)
     // Main game loop
     while (currentState != EXIT) 
     {
-        //Update game 
+        //Update game screen state 
         switch(currentScreen)
 	{
           case MENU:
@@ -66,42 +81,153 @@ int main(void)
 	  } break;
 	  
 	  default: break;
-	}	
+        }	
+       
+	//Update player move state 
+        switch(playerMoveState)
+	{
+          case NOMOVE:
+          {
+ 	    //move forward 	
+	    if(IsKeyDown(KEY_W)) {
+	      playerMoveState = MOVEFORWARD;
+	    }
         
-        //move forward 	
-	if(IsKeyDown(KEY_W)) {
-	  playerPos.x = playerPos.x + moveUp.x;
-	  playerPos.y = playerPos.y + moveUp.y; 
+	    //move down
+            if(IsKeyDown(KEY_S)) {
+	      playerMoveState = MOVEBACKWARD;
+	    }
+        
+           //move right	
+           if(IsKeyDown(KEY_D)) {
+	      playerMoveState = MOVERIGHT;
+	    }
+	
+	   //move left	
+           if(IsKeyDown(KEY_A)) {
+	      playerMoveState = MOVELEFT;
+	    }
+  
+	 } break;
+	 
+	  case MOVEFORWARD:
+          {
+ 	    //move forward 	
+	    if(IsKeyDown(KEY_W)) {
+	      playerPos.x = playerPos.x + moveUp.x;
+	      playerPos.y = playerPos.y + moveUp.y; 
+	    }
+        
+	    //move down
+            if(IsKeyDown(KEY_S)) {
+	      playerMoveState = MOVEBACKWARD;
+	    }
+        
+           //move diag right	
+           if(IsKeyDown(KEY_D)) {
+	      playerMoveState = MOVERIGHT;
+	    }
+	
+	   //move diag left	
+           if(IsKeyDown(KEY_A)) {
+	      playerMoveState = MOVELEFT;
+	    }
+ 	  
+	   //no move 	
+           if(IsKeyUp(KEY_W)) {
+	      playerMoveState = NOMOVE;
+	    }
+ 
+	  } break;
+
+	  case MOVEBACKWARD:
+          {
+ 	    //move forward 	
+	    if(IsKeyDown(KEY_W)) {
+	      playerMoveState = MOVEFORWARD;
+	    }
+        
+	    //move down
+            if(IsKeyDown(KEY_S)) {
+	      playerPos.x = playerPos.x + moveDown.x;
+	      playerPos.y = playerPos.y + moveDown.y;
+	    }
+         
+	   //no move 	
+	    if (IsKeyUp(KEY_S)){  
+	      playerMoveState = NOMOVE;
+	    }
+ 	   
+	  } break;
+
+
+	  case MOVELEFT:
+          {
+ 	    //move forward 	
+	    if(IsKeyDown(KEY_W)) {
+	      playerMoveState = MOVEFORWARD;
+	    }
+        
+	    //move down
+            if(IsKeyDown(KEY_S)) {
+	      playerMoveState = MOVEBACKWARD;
+	    }
+        
+           //move right	
+           if(IsKeyDown(KEY_D)) {
+	      playerMoveState = MOVERIGHT;
+	    }
+	
+	   //move left	
+           if(IsKeyDown(KEY_A)) {
+              playerPos.x = playerPos.x + moveLeft.x; 
+  	      playerPos.y = playerPos.y + moveLeft.y; 	  
+	    }
+ 	  
+	   //no move 	
+           if(IsKeyUp(KEY_A)) {
+	      playerMoveState = NOMOVE;
+	    }
+ 
+	  } break;
+
+	  case MOVERIGHT:
+          {
+ 	    //move forward 	
+	    if(IsKeyDown(KEY_W)) {
+	      playerMoveState = MOVEFORWARD;
+	    }
+        
+	    //move down
+            if(IsKeyDown(KEY_S)) {
+	      playerMoveState = MOVEBACKWARD;
+	    }
+        
+           //move right	
+           if(IsKeyDown(KEY_D)) {
+              playerPos.x = playerPos.x + moveRight.x;
+  	      playerPos.y = playerPos.y + moveRight.y; 	  
+	    }
+	
+	   //move left	
+           if(IsKeyDown(KEY_A)) {
+	      playerMoveState = MOVELEFT;
+	    }
+ 	  
+	   //no move 	
+           if(IsKeyUp(KEY_D)) {
+	      playerMoveState = NOMOVE;
+	    }
+ 
+	  } break;
+
+	  default: break;
+	 
 	}
         
-	//move down
-        if(IsKeyDown(KEY_S)) {
-	  playerPos.x = playerPos.x + moveDown.x;
-	  playerPos.y = playerPos.y + moveDown.y;
-	}
-        
-        //move right	
-        if(IsKeyDown(KEY_D)) {	
-          playerPos.x = playerPos.x + moveRight.x;
-  	  playerPos.y = playerPos.y + moveRight.y; 	  
-	}
-	
-	//move left	
-        if(IsKeyDown(KEY_A)) {	
-          playerPos.x = playerPos.x + moveLeft.x; 
-  	  playerPos.y = playerPos.y + moveLeft.y; 	  
-	}
-	
-	//handle normalising the playerRotationState value to between 0-360 
-	if (playerRotationState >= 360){
-	  playerRotationState -= 360;
-	}
-	else if (playerRotationState < 0){
-	  playerRotationState  += 360;
-	}
-	
-	
-	printf("%f\n",  playerRotationState); 
+       
+	//print the player's current move state
+	printf("%s \n", getPlayerMoveStateString(playerMoveState));
 
 	//Draw
         BeginDrawing();
@@ -117,8 +243,7 @@ int main(void)
 	    case GAMEPLAY:
 	    {
 	      ClearBackground(RAYWHITE);
-	      DrawTextureEx(player, playerPos, playerRotationState, playerScale, WHITE); 
-	      //DrawTexture(player, playerPos.x, playerPos.y, WHITE); 
+	      DrawTexture(player, playerPos.x, playerPos.y, WHITE); 
 	      DrawTexture(asteroid, 100.f, 100.f, WHITE);
 	      DrawTexture(asteroidMedium, 200.f, 600.f, WHITE);
 	      DrawTexture(asteroidLarge, 920.f, 510.f, WHITE);
