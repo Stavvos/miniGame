f074d2be998f0d8ff5fb40e74dc10f596ef2f94c
liam henry <liam_burridgeuc@hotmail.com>
2025-11-09 21:11:04 +1100

    REFACTOR: removed unessacery comments within level.c

--- level.c
+++ level.c
@@ -4,7 +4,6 @@ void levelChangeHandler(struct Asteroid** head, struct Game* game, struct Screen
 {
   struct Asteroid* current = *head;
   
-  //if no asteroids in linked list
   if(current == NULL) 
   {
     if(game->level < game->MAXLEVEL)
@@ -20,7 +19,6 @@ void levelChangeHandler(struct Asteroid** head, struct Game* game, struct Screen
       game->level = 0;
     }
   }
-
 }
 
 void levelHandler(struct Game* game, struct Player* player, struct Bullet bullets[], struct Asteroid** asteroidHead, char* levelFileNames[], struct Explosion explosions[])
@@ -29,80 +27,37 @@ void levelHandler(struct Game* game, struct Player* player, struct Bullet bullet
   {
     case RESETLEVEL:
     {
-      //delete the linked list
       freeAsteroidList(*asteroidHead);
-
-      //initialise the head
       *asteroidHead = NULL;
-
-      //initialise the linked list
       initialiseLevel(levelFileNames[game->level], asteroidHead);
-           
-      //reset player's position
       player->playerPos = (Vector2){300.f, 280.f};
-
-      //reset bullets
       deactivateBullets(bullets, game);
-      
-      //reset explosions
       resetExplosions(explosions);
-
-      //set game state to playing
       game->gameState = PLAYING;
-      
     } break;
 
     case NEXTLEVEL:
     {
-      //delete the linked list
       freeAsteroidList(*asteroidHead);
-
-      //initialise the head
       *asteroidHead = NULL;
-
-      //initialise the linked list
       initialiseLevel(levelFileNames[game->level], asteroidHead);
-      
-      //reset player's position
       player->playerPos = (Vector2){300.f, 280.f};
-
-      //reset bullets
       deactivateBullets(bullets, game);
-      
-      //reset explosions
       resetExplosions(explosions);
-
-      //set game state to playing
       game->gameState = PLAYING;
-    
     } break;
 
     case ENDGAME:
     {
-      //delete the linked list
       freeAsteroidList(*asteroidHead);
-
-      //initialise the head
       *asteroidHead = NULL;
-
-      //initialise the linked list
       initialiseLevel(levelFileNames[game->level], asteroidHead);
-      
-      //reset player's position and score
       player->playerPos = (Vector2){300.f, 280.f};
-
-      //reset bullets
       deactivateBullets(bullets, game);
-      
-      //reset explosions
       resetExplosions(explosions);
-
-      //set game state to playing
       game->gameState = PLAYING;
-    
     } break;
     
       default: break;
   }
-
 }
