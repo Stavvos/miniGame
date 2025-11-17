bee7bf55d9a987c3c3a14d57c11efe859787a7e0
liam henry <liam_burridgeuc@hotmail.com>
2025-11-17 19:33:30 +1100

    NEWFEATURE: the asteroids now don't stack ontop of each other. Used separate function from boid alogorithm

--- asteroidMovement.c
+++ asteroidMovement.c
@@ -1,5 +1,30 @@
 #include "types.h"
 #include "math.h"
+#include "raymath.h"
+
+Vector2 separateAsteroids(struct Asteroid* head, struct Asteroid* asteroid)
+{
+  Vector2 result = {0, 0};
+  float distanceLimit = 20.0;
+  
+  struct Asteroid* current = head;
+
+  while (current != NULL)
+  {
+    if (current != asteroid)
+    {
+      float distance = Vector2Distance(asteroid->position, current->position);
+      
+      if (distance < distanceLimit)
+      {
+        result.x -= current->position.x - asteroid->position.x; 
+        result.y -= current->position.y - asteroid->position.y; 
+      }
+    }
+    current = current->next;
+  }
+  return result;
+}
 
 void moveAsteroids(struct Player* player, struct Screen* screen, struct Game* game, struct Asteroid* head)
 {
@@ -10,6 +35,10 @@ void moveAsteroids(struct Player* player, struct Screen* screen, struct Game* ga
 
     while(current != NULL)
     {
+      Vector2 adjustment = separateAsteroids(head, current);
+      adjustment.x *= 0.1;
+      adjustment.y *= 0.1;
+
       current->direction.x = player->playerPos.x - current->position.x;
       current->direction.y = player->playerPos.y - current->position.y;
 
@@ -17,13 +46,9 @@ void moveAsteroids(struct Player* player, struct Screen* screen, struct Game* ga
     
       current->direction.x /= current->hypotenuse;
       current->direction.y /= current->hypotenuse;
-     
-      //minimise the asteroids stacking ontop of one another 
-      float randomOffsetX = ((rand() % 100) - 50) * 0.01;  
-      float randomOffsetY = ((rand() % 100)- 50) * 0.01;  
       
-      current->position.x += (randomOffsetX + current->direction.x) * game->deltaTime * current->speed;
-      current->position.y += (randomOffsetY + current->direction.y) * game->deltaTime * current->speed;
+      current->position.x += (adjustment.x + current->direction.x) * game->deltaTime * current->speed;
+      current->position.y += (adjustment.y + current->direction.y) * game->deltaTime * current->speed;
 
       current->hitBox.x = current->position.x;
       current->hitBox.y = current->position.y;
@@ -31,5 +56,4 @@ void moveAsteroids(struct Player* player, struct Screen* screen, struct Game* ga
       current = current->next;
     }
   }
-
 }
